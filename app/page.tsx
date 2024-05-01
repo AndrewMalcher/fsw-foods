8d25d0dd5322446c3fedf69c8b7450a7bace937a
andrew malcher <andrew.malcher.r@gmail.com>
2024-04-30 20:08:37 -0400

    feat: add initial Search icon and input

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import { Search } from "lucide-react";
 import Header from "./_components/header";
 
 const Home = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <div className="px-5 pt-6">
+        <Search />
+      </div>
+    </>
+  );
 };
 
 export default Home;
