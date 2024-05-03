cfa177a40f96b991fbc235eb3b09466d4ba9f9fd
andrew malcher <andrew.malcher.r@gmail.com>
2024-05-02 20:26:57 -0400

    feat: add category item and list components

--- app/_components/search.tsx
+++ app/_components/search.tsx
@@ -5,7 +5,7 @@ import { Input } from "./ui/input";
 const Search = () => {
   return (
     <div className="flex gap-4">
-      <Input placeholder="Buscar restaurantes" className="border-none px-5" />
+      <Input placeholder="Pesquise restaurantes" className="border-none" />
       <Button size="icon">
         <SearchIcon size={20} />
       </Button>
