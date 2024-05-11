d2c5b142c73b36ba0bab31b3f91a6ef012715643
andrew malcher <andrew.malcher.r@gmail.com>
2024-05-10 21:47:10 -0400

    feat: add recommended restaurants section to home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import { Button } from "./_components/ui/button";
 import { ChevronRightIcon } from "lucide-react";
 import { db } from "./lib/prisma";
 import PromoBanner from "./_components/promo-banner";
+import RestaurantList from "./_components/restaurant-list";
 
 const Home = async () => {
   const products = await db.product.findMany({
@@ -41,7 +42,7 @@ const Home = async () => {
         />
       </div>
 
-      <div className="space-y-4 pt-6">
+      <div className="space-y-4 py-6">
         <div className="flex items-center justify-between px-5">
           <h2 className="font-semibold">Pedidos Recomendados</h2>
           <Button
@@ -59,7 +60,21 @@ const Home = async () => {
       </div>
 
       <div className="px-5 pt-6">
-        <PromoBanner src="/promobanner-02.png" alt="a partir de R$17,!" />
+        <PromoBanner src="/promobanner-02.png" alt="a partir de R$17,90!" />
+      </div>
+
+      <div className="space-y-4 py-6">
+        <div className="flex items-center justify-between px-5">
+          <h2 className="font-semibold">Restaurantes Recomendados</h2>
+          <Button
+            variant="ghost"
+            className="h-fit p-0 text-primary hover:bg-transparent"
+          >
+            Ver todos
+            <ChevronRightIcon size={16} />
+          </Button>
+        </div>
+        <RestaurantList />
       </div>
     </>
   );
