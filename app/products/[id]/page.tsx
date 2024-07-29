1d45f44ed88843bda009fce1f6162bcc0b324b09
AndrewMalcher <andrew.malcher.r@gmail.com>
2024-07-29 16:32:45 -0400

    feat: fix promobanner-01

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,4 +1,8 @@
+import { Button } from "@/app/_components/ui/button";
 import { db } from "@/app/lib/prisma";
+import { ChevronLeftIcon } from "lucide-react";
+import Image from "next/image";
+import { notFound } from "next/navigation";
 
 interface ProductPageProps {
   params: {
@@ -13,7 +17,27 @@ const ProductPage = async ({ params: { id } }: ProductPageProps) => {
     },
   });
 
-  return <div>Product Page {id}</div>;
+  if (!product) {
+    return notFound();
+  }
+
+  return (
+    <div>
+      {/* IMAGEM */}
+      <div className="relative h-[360px] w-full">
+        <Image
+          src={product.imageUrl}
+          alt={product.name}
+          fill
+          className="object-cover"
+        />
+
+        <Button>
+          <ChevronLeftIcon />
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default ProductPage;
